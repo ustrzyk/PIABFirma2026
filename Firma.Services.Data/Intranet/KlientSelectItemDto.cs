5ed54bdc2f884e89075a66b9a72038e5afdc49e3
ustrzyk <165511442+ustrzyk@users.noreply.github.com>
2026-06-15 18:01:30 +0200

    Migracja doServisow

---
+++ Firma.Services.Data/Intranet/KlientSelectItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Firma.Services.Data.Intranet
+{
+    public class KlientSelectItemDto
+    {
+        public int IdKlienta { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+    }
+}
